e0f2086992856b7ecdd83da57ce7cb3eb65819e6
minako yoshino <minako.yoshino@kaminashi.com>
2024-08-05 21:50:52 +0900

    section 56 まで

--- main.go
+++ main.go
@@ -4,21 +4,34 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Printf("need a port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+
 	// context.Background() で空のコンテキストを生成
-	if err := run(context.Background()); err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
@@ -31,7 +44,7 @@ func run(ctx context.Context) error {
 	eg.Go(func() error {
 		// クロージャの特性により、この無名関数は s への参照を保持し、
 		// 実行時にその値にアクセスすることができる
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 		// http.ErrServerClosed は http.Server.Shutdown() が正常に終了したことを示すので異常ではない
 		err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
