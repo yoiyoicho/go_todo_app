883421c181ef6ce1f7293e8cfaea85473e04d8c9
minako yoshino <minako.yoshino@kaminashi.com>
2024-08-16 18:32:18 +0900

    add gabage

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+// GolangCI-Lint のチェック用
+const Url = "http://example.com"
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Printf("need a port number\n")
