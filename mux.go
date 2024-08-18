5a1e3b3d13bfab62ecdc0c2892baa2ce3b9bb9ef
minako yoshino <minako.yoshino@kaminashi.com>
2024-08-18 12:18:53 +0900

    section70まで

--- mux.go
+++ mux.go
@@ -1,12 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+	"github.com/yoiyoicho/go_todo_app/handler"
+	"github.com/yoiyoicho/go_todo_app/store"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	// Postの第2引数はhttp.HandlerFunc型の関数を受け取る
+	// なぜhttp.HandlerFunc(at.ServeHTTP)としなくてもOKなのか？
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
 	return mux
 }
